03ccfa75864065ff10a92531d27af92fe3ad0e5b
yugiyx <yugiyx@yugiyxdeMacBook-Pro.local>
2018-03-17 22:08:19 +0800

    Delete old codes

--- learning_log/learning_logs/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Topic
-
-
-class TopicForm(forms.ModelForm):
-    class Meta:
-        model = Topic
-        fields = ['text']
-        labels = {'text': ''}
