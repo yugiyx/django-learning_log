9a0255421894233d5fe350877100a3a1860f3efb
yugiyx <yugiyx@yugiyxdeMacBook-Pro.local>
2018-03-25 18:02:55 +0800

    Finsh web all function

--- src/learning_logs/views.py
+++ src/learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -11,45 +12,54 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """显示所有的主题"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
-        """未提交数据：创建新表单"""
+        # 未提交数据：创建新表单
         form = TopicForm()
     else:
-        """POST提交的数据，对数据进行处理"""
+        # POST提交的数据，对数据进行处理
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """添加新条目"""
     topic = Topic.objects.get(id=topic_id)
 
     if request.method != 'POST':
-        """未提交数据：创建新表单"""
+        # 未提交数据：创建新表单
         form = EntryForm()
     else:
-        """POST提交的数据，对数据进行处理"""
+        # POST提交的数据，对数据进行处理
         form = EntryForm(request.POST)
         if form.is_valid():
             new_entry = form.save(commit=False)
@@ -61,11 +71,14 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """编辑条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-
+    # 确认请求主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         # 初次请求 使用当前条目填充表单
         form = EntryForm(instance=entry)
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
+def register(request):
+    if request.method != 'POST':
+        # 显示空表单
+        form = UserCreationForm()
+    else:
+        # 处理填写好的表单
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            # 让用户自动登录，并重定向到主页
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            authenticated_user = authenticate(username=username, password=password)
+            # authenticated_user = authenticate(
+            #     request, user=new_user.username,
+            #     password=request.POST['password1'])
+            print(type(authenticated_user),authenticated_user)
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
